636c9617b942bdf6f525ab7d57709fc636d0454f
Camilo Meneses <kamilomeneses@gmail.com>
2023-10-04 19:38:29 -0500

    add Example of docker compose with Postgres simple database

---
+++ src/test/java/dev/camilo/dbdemo/DbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.camilo.dbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
